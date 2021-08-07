bca5b6d2d3d6431478e719f1014ff6fd5cd85cd6
pdx mbridges <pdxmbridges@gmail.com>
2021-08-07 14:38:06 -0700

    seems to be going

---
+++ raspberrypython/test.py
@@ -0,0 +1,17 @@
+import os
+import time
+import sys
+import threading
+import pygame, sys
+from pygame.locals import *
+from time import ctime, sleep
+pygame.init
+
+cmd = "sudo raspistill -t 0"
+os.system (cmd)
+
+
+
+print ("hello world")
+
+
